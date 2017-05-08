5a7060a08bfce06655001199420ce0d84831fc2e
Aurelie Felsen <aurelie.felsen@gmail.com>
2017-05-08 03:48:06 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -3,25 +3,32 @@
 #with the correct path to this directory on your computer
 
 # my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
+# "Go Go GO" => 'Users/Lili/jukebox-cli/audio/Emerald-Park/01.mp3',
+# "LiberTeens" => 'Users/Lili/jukebox-cli/audio/Emerald-Park/02.mp3',
+# "Hamburg" =>  'Users/Lili/jukebox-cli/audio/Emerald-Park/03.mp3',
+# "Guiding Light" => 'Users/Lili/jukebox-cli/audio/Emerald-Park/04.mp3',
+# "Wolf" => 'Users/Lili/jukebox-cli/audio/Emerald-Park/05.mp3',
+# "Blue" => 'Users/Lili/jukebox-cli/audio/Emerald-Park/06.mp3',
+# "Graduation Failed" => 'Users/Lili/jukebox-cli/audio/Emerald-Park/07.mp3'
 # }
 
 def help
   #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:
+        - help : displays this help message
+        - list : displays a list of songs you can play
+        - play : lets you choose a song to play
+        - exit : exits this program"
 end
 
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
+  my_songs.keys.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
 end
 
 
@@ -33,13 +40,32 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+  puts "Please enter a song name:"
+  users_choice = gets.chomp
+  if my_songs.keys.include?(users_choice)
+    system 'open <my_songs[users_choice].value>'
+  else
+    puts "Invalid input, please try again."
+  end
 end
 
 def exit_jukebox
-  #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
 def run(my_songs)
-  #this method is the same as in jukebox.rb
+  help
+  puts "Please enter a command:"
+  users_input = gets.chomp
+  while users_input != "exit"
+    if users_input == "list"
+      list(songs)
+    elsif users_input == "play"
+      play(songs)
+    elsif users_input == "help"
+      help
+    else
+      puts "Invalid input, please try again."
+    end
+  end
 end
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,54 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:
+        - help : displays this help message
+        - list : displays a list of songs you can play
+        - play : lets you choose a song to play
+        - exit : exits this program"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  users_choice = gets.chomp
+  if songs.include?(users_choice)
+    puts "Playing #{users_choice}"
+  elsif (1..9).to_a.include?(users_choice.to_i)
+    puts "Playing #{songs[users_choice.to_i - 1]}"
+  else
+    puts "Invalid input, please try again."
+  end
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+  users_input = gets.chomp
+    if users_input == "exit"
+      exit_jukebox
+    end
+
+  while users_input != "exit"
+    if users_input == "list"
+      list(songs)
+    elsif users_input == "play"
+      play(songs)
+    elsif users_input == "help"
+      help
+    else
+      puts "Invalid input, please try again."
+    end
+    users_input = gets.chomp
+  end
+end
